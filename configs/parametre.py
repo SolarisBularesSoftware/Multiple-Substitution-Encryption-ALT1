924f73d7153e3c23b93f498a19ed02779caa1ba9
FLOW LORD <56801474+flowlord@users.noreply.github.com>
2022-08-15 19:50:38 +0200

    Ajoute de la fonction gen_para_aleatoire 2

--- configs/parametre.py
+++ configs/parametre.py
@@ -14,7 +14,6 @@ IV) Je définie mes carcatères spéciaux
 	ces caractères auront un groupe de carcatère d'une longeur plus petit
 
 ---------------------------------------------------------------------------
-libre à vous de modifier ce fichier !
 faite votre propre version et le montrer à personne !
 Regénèrer vos clés de chiffrement si vous modifier ce fichier !!
 
@@ -24,8 +23,7 @@ from string import ascii_lowercase
 from random import shuffle
 import os
 
-espace = " "
-carac_sub = list(ascii_lowercase+espace)
+carac_sub = list(ascii_lowercase+' ')
 len_carac_sub = len(carac_sub)
 
 name = "configs/all.carac"
@@ -38,7 +36,7 @@ groupe_a = groupe_caracteres_initial[:milieu]
 groupe_b = groupe_caracteres_initial[milieu:]
 
 
-carac_special = 'esuwfdntzcapv'+espace
+carac_special = 'esuwfdntzcapv '
 
 # Cette ligne est purement esthétique
 groupe_b = groupe_b+''.join(carac_sub)*50+carac_special*50
@@ -49,6 +47,3 @@ longeur_carac_special = (2,4)
 nombre_cle = (10,30)
 
 mini,maxi = 100,300
-
-
-
