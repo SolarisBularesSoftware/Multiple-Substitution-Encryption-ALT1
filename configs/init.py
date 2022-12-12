b3de833bc87dae140125e79281fa05a3f869f232
FLOW LORD <56801474+flowlord@users.noreply.github.com>
2022-12-12 03:30:28 +0100

    Add files via upload

--- configs/init.py
+++ configs/init.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from random import randint
 from string import ascii_lowercase,ascii_uppercase, ascii_letters, digits, punctuation
 import json
 import os.path
 
 accent = 'ÄÀÂÉÈÊËÎÏÔÙÛÜÇàâéèêëîïôùûüç'
 
-file = open('configs/setting.json', 'r')
+nom_fichier_parametre = "configs/setting.json"
+
+if os.path.exists(nom_fichier_parametre) is False:
+	raise Exception(f'Erreur --> Fichier {nom_fichier_parametre} introuvable')
+
+file = open(nom_fichier_parametre, 'r')
 data = json.load(file)
 file.close()
 
 carac_sub = data['cipher']
+
+# 3 options: ascii_lowercase, ascii_uppercase, ascii_letters
 
 if data['cipher'] == 'ascii_lowercase':
 	carac_sub = ascii_lowercase
@@ -20,7 +28,7 @@ elif data['cipher'] == 'ascii_uppercase':
 elif data['cipher'] == 'ascii_letters':
 	carac_sub = ascii_letters
 else:
-	raise Exception('uknown cipher option')
+	raise Exception('[ Erreur --> setting.json: Option incconue ]')
 
 
 if data["cipher punctuation"] == True:
@@ -34,10 +42,10 @@ if data["cipher accent"] == True:
 
 len_carac_sub = len(carac_sub)
 
-name = data['substitue with']
+fichier_jeux_carac = data['substitue with']
 
-if os.path.exists(name) is False:
-	raise Exception('carac file name not found')
+if os.path.exists(fichier_jeux_carac) is False:
+	raise Exception('Erreur --> Fichier de jeux de caractères introuvable')
 
 
 len_caractere = data['len_caractere']
@@ -46,17 +54,15 @@ nombre_cle = data['nombre_cle']
 mini,maxi = data['mini'],data['maxi']
 
 
-groupe_caracteres_initial = "".join(open(name,'r',encoding='utf-8').readlines())
+groupe_caracteres_initial = "".join(open(fichier_jeux_carac,'r',encoding='utf-8').readlines())
 milieu = int(len(groupe_caracteres_initial)/2)
 groupe_a = groupe_caracteres_initial[:milieu]
 groupe_b = groupe_caracteres_initial[milieu:]
 
-carac_special = 'esantirulodcp '
+carac_special = 'esantirulodcpxwv '
 
 if data['confusion'] == True:
-	groupe_b = groupe_b+carac_sub*10+carac_special*10
-
-
+	groupe_b = groupe_b+carac_sub*randint(5,100)+carac_special*randint(5,50)
 
 
 
